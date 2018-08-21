57d2e8b67ae74d4e6d5f1580fa98ccd3a2e93b32
Abraham <wits73@hotmail.com>
2018-08-20 22:10:22 -0400

    add tdd api

--- tdd_basic/index.js
+++ tdd_basic/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-Parser');
 
 let users = [
     { id: 1, name: 'alice' },
@@ -9,11 +10,72 @@ let users = [
 ];
 
 app.use(morgan('dev'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/', (req, res) => {
-    res.json(users);
+app.get('/users', function (req, res) {
+    req.query.limit = req.query.limit || 10;
+    const limit = parseInt(req.query.limit, 10);
+
+    if (Number.isNaN(limit)) {
+        return res.status(400).end();
+    }
+    res.json(users.slice(0, limit));
+});
+
+app.get('/users/:id', function (req, res) {
+    const id = parseInt(req.params.id, 10);
+    if (Number.isNaN(id)) return res.status(400).end();
+
+    const user = users.filter((user) => user.id === id)[0];
+    if (!user) return res.status(404).end();
+
+    res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (Number.isNaN(id)) return res.status(400).end();
+
+    users = users.filter(user => user.id !== id);
+    res.status(204).end();
+});
+
+app.post('/users', (req, res) => {
+    const name = req.body.name;
+    if (!name) return res.status(400).end();
+
+    const isConflict = users.filter(user => user.name === name).length;
+    if (isConflict) return res.status(409).end();
+
+    const id = Date.now();
+    const user = { id, name };
+    users.push(user);
+    res.status(201).json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (Number.isNaN(id)) return res.status(400).end();
+
+    const name = req.body.name;
+    if (!name) return res.status(400).end();
+
+    const isConflict = users.filter(user => user.name === name).length;
+    if (isConflict) return res.status(409).end();
+
+    const user = users.filter(user => user.id === id)[0];
+    if (!user) return res.status(404).end();
+
+    user.name = name;
+
+    res.json(user);
 });
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+
+
+module.exports = app;
--- tdd_basic/index.spec.js
+++ tdd_basic/index.spec.js
@@ -1,7 +1,163 @@
-const assert = require('assert');
+const request = require('supertest');
+const should = require('should');
+const app = require('./index');
 
 describe('GET /users', () => {
-    it('Return Array', () => {
-        assert.equal(1,1);
+    describe('Success', () => {
+        it('Response array with user object', (done) => {
+            request(app)
+                .get('/users')
+                .end((err, res) => {
+                    res.body.should.be.instanceOf(Array);
+                    done();
+                });
+        });
+
+        it('Response max counts of array', (done) => {
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res) => {
+                    res.body.should.have.lengthOf(2)
+                    done();
+                });
+        });
     });
-})
+    describe('Failed', () => {
+        it('Response if limit is not number then 400', (done) => {
+            request(app)
+                .get('/users?limit=two')
+                .expect(400)
+                .end(done);
+        })
+    })
+});
+
+
+describe('GET /users/:id', () => {
+    describe('Success', () => {
+        it('Return user object id is 1', (done) => {
+            request(app)
+                .get('/users/1')
+                .end((err, res) => {
+                    res.body.should.have.property('id', 1);
+                    done();
+                });
+        });
+    });
+    describe('Failed', () => {
+        it('Response 400, if id is not number', (done) => {
+            request(app)
+                .get('/users/one')
+                .expect(400)
+                .end(done);
+        });
+        it('Response 404, if there is no exist id', (done) => {
+            request(app)
+                .get('/users/999')
+                .expect(404)
+                .end(done);
+        });
+    })
+});
+
+describe('DELETE /users/:id', () => {
+    describe('Success', () => {
+        it('Return 204', (done) => {
+            request(app)
+                .delete('/users/1')
+                .expect(204)
+                .end(done);
+        });
+    });
+    describe('Failed', () => {
+        it('Response 400, if id is not number', (done) => {
+            request(app)
+                .get('/users/one')
+                .expect(400)
+                .end(done);
+        });
+    });
+});
+
+describe('POST /users', () => {
+    describe('Success', () => {
+        let name = 'daniel', body;
+        before(done => {
+            request(app)
+                .post('/users')
+                .send({ name })
+                .expect(201)
+                .end((err, res) => {
+                    body = res.body;
+                    done();
+                });
+        });
+        it('Return user that created ', () => {
+            body.should.have.property('id');
+        });
+        it('Return name that inserted ', () => {
+            body.should.have.property('name', name);
+        });
+    });
+    describe('Failed', () => {
+        it('Response 400, if there is no parameter', (done) => {
+            request(app)
+                .post('/users')
+                .send({})
+                .expect(400)
+                .end(done)
+        });
+        it('Response 400, if there is duplicated on name', (done) => {
+            request(app)
+                .post('/users')
+                .send({ name: 'daniel' })
+                .expect(409)
+                .end(done)
+        });
+    });
+});
+
+
+describe('PUT /users/:id', () => {
+    describe('Success', () => {
+        it('Return modified name ', (done) => {
+            let name = 'chally';
+            request(app)
+                .put('/users/3')
+                .send({ name })
+                .end((err, res) => {
+                    res.body.should.have.property('name', name);
+                    done();
+                });
+        });
+    });
+    describe('Failed', () => {
+        it('Response 400, if id is not number', (done) => {
+            request(app)
+                .put('/users/one')
+                .expect(400)
+                .end(done)
+        });
+        it('Response 400, if there is no name', (done) => {
+            request(app)
+                .put('/users/1')
+                .send({})
+                .expect(400)
+                .end(done);
+        });
+        it('Response 404, if there is no user', (done) => {
+            request(app)
+                .put('/users/999')
+                .send({ name: 'foo' })
+                .expect(404)
+                .end(done);
+        });
+        it('Response 409, if there is duplicated on name', (done) => {
+            request(app)
+                .put('/users/2')
+                .send({ name: 'bek' })
+                .expect(409)
+                .end(done);
+        });
+    });
+});
